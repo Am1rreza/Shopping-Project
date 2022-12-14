56c9f98888a2e07b2f06aafe697d5da8c9f8ed4a
Amirreza <amirreza.m13825@gmail.com>
2022-12-14 12:22:56 +0330

    getAddToCartBtns function in UI class added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { productsData } from "./products.js";
 const cartBtn = document.querySelector(".cart-btn");
-const cart = document.querySelector(".cart");
+const cartModal = document.querySelector(".cart");
 const backDrop = document.querySelector(".backdrop");
 const closeModal = document.querySelector(".cart-item-confirm");
 const productsDOM = document.querySelector(".products-center");
+
+const cart = [];
 
 // get products
 class Products {
@@ -20,22 +22,43 @@ class UI {
     products.forEach((item) => {
       result += `<section class="product">
       <div class="img-container">
-        <img
-          class="product-img"
-          src="${item.imageUrl}"
+      <img
+      class="product-img"
+      src="${item.imageUrl}"
           alt="p-1"
-        />
-      </div>
-      <div class="products-desc">
-        <p class="products-title">${item.title}</p>
-        <p class="products-price">${item.price}$</p>
-      </div>
-      <button class="add-to-cart btn" data-id=${item.id}>
-        <i class="fas fa-shopping-cart"></i>
-        Add to cart
-      </button>
-    </section>`;
+          />
+          </div>
+          <div class="products-desc">
+          <p class="products-title">${item.title}</p>
+          <p class="products-price">${item.price}$</p>
+          </div>
+          <button class="add-to-cart btn" data-id=${item.id}>
+          <i class="fas fa-shopping-cart"></i>
+          Add to cart
+          </button>
+          </section>`;
       productsDOM.innerHTML = result;
+    });
+  }
+
+  getAddToCartBtns() {
+    const addToCartBtn = document.querySelectorAll(".add-to-cart");
+    const buttons = [...addToCartBtn];
+
+    buttons.forEach((btn) => {
+      const id = btn.dataset.id;
+      // check if this product id is in cart or not
+      const isInCart = cart.find((p) => p.id === id);
+      if (isInCart) {
+        btn.innerText = "In Cart";
+        btn.disabled = true;
+      }
+      // add event listener to btn
+      btn.addEventListener("click", (e) => {
+        // get product from products
+        // add product to cart
+        // save cart to local storage
+      });
     });
   }
 }
@@ -56,6 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //display products
   const ui = new UI();
   ui.displayProducts(productsData);
+  ui.getAddToCartBtns();
 
   // save products
   Storage.saveProducts(productsData);
@@ -64,14 +88,14 @@ document.addEventListener("DOMContentLoaded", () => {
 // cart items modal
 function showModalFunction() {
   backDrop.style.display = "block";
-  cart.style.opacity = "1";
-  cart.style.top = "35%";
+  cartModal.style.opacity = "1";
+  cartModal.style.top = "35%";
 }
 
 function closeModalFunction() {
   backDrop.style.display = "none";
-  cart.style.opacity = "0";
-  cart.style.top = "-100%";
+  cartModal.style.opacity = "0";
+  cartModal.style.top = "-100%";
 }
 
 cartBtn.addEventListener("click", showModalFunction);
